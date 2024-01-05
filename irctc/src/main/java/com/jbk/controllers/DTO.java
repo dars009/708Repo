2544e560bac66e5434bb5fbd9f35ccfffa0801a6
gaurav <gauravchimote45@gmail.com>
2024-01-05 10:39:45 +0530

    Merge branch 'feature/GAURAV_02' of https://github.com/dars009/708Repo.git into feature/GAURAV_02

--- irctc/src/main/java/com/jbk/controllers/DTO.java
+++ irctc/src/main/java/com/jbk/controllers/DTO.java
@@ -3,6 +3,7 @@ package com.jbk.controllers;
 public class DTO {
 	public static void main(String[] args) {
 		System.out.println("hello this is gaurav_02 from nagpur");
+		System.out.println("7558724427");
 	}
 
 }
