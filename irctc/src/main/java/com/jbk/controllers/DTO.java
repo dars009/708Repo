bc6ba80f6c00ca9610aae0ab29aeec0026abf298
gaurav <gauravchimote45@gmail.com>
2024-01-05 10:41:14 +0530

    i am adding my number check it.....

--- irctc/src/main/java/com/jbk/controllers/DTO.java
+++ irctc/src/main/java/com/jbk/controllers/DTO.java
@@ -4,6 +4,7 @@ public class DTO {
 	public static void main(String[] args) {
 		System.out.println("hello this is gaurav_02 from nagpur");
 		System.out.println("7558724427");
+		System.out.println("7558724427");
 	}
 
 }
