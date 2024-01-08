2a75f538f1269c7832cfa9f5f936618562e45655
shree <shree@LAPTOP-L4E3D6U1>
2024-01-08 23:05:05 +0530

    dev_Url_SDLC

---
+++ irctc/src/main/java/com/jbk/controllers/Shreeyog_dev_SDLC_Url.java
@@ -0,0 +1,17 @@
+package com.jbk.controllers;
+
+public class Shreeyog_dev_SDLC_Url {
+
+	public static void main(String[] args) 
+	
+	
+	{
+
+		//url :
+		
+		System.out.println("This is the url use can use to access SDLC Model "+"https://coggle.it/diagram/YRoIFm94SiMDMbXK/t/project-management-tool-common-for-all-ibm-control-center");
+		
+		
+	}
+
+}
