1b0914f49644fe760435f6830900b3c6b8e0d5a6
shree <shree@LAPTOP-L4E3D6U1>
2024-01-08 23:18:32 +0530

    Shreeyog_main_Url_SDLC

---
+++ irctc/src/main/java/com/jbk/controllers/Shreeyog_Main_SDLC_Url.java
@@ -0,0 +1,13 @@
+package com.jbk.controllers;
+
+public class Shreeyog_Main_SDLC_Url {
+
+	public static void main(String[] args) 
+	{
+
+		System.out.println("CP_this url into your browser to redirect SDLC public diagram  "+"https://coggle.it/diagram/YRoIFm94SiMDMbXK/t/project-management-tool-common-for-all-ibm-control-center");
+		
+		
+	}
+
+}
