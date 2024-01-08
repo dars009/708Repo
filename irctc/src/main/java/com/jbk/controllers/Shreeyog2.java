ab24cdc425ea7a5bf31e9a25f3b809ffa81a3adc
shree <shree@LAPTOP-L4E3D6U1>
2024-01-08 18:23:34 +0530

    Third commit GfG Prg Soln.../

---
+++ irctc/src/main/java/com/jbk/controllers/Shreeyog2.java
@@ -0,0 +1,30 @@
+package com.jbk.controllers;
+
+//#GFG PRG........./
+
+// Precise Format - Java...../
+
+/*Input:
+5.43 2.653
+
+Output:
+0.20467396 
+0.205
+*/
+public class Shreeyog2 {
+
+	public static void main(String[] args)
+	
+	{
+		
+		float a=543;
+		float b=2653;
+        float result=a/b;
+		
+		System.out.println(result +" ");
+		System.out.format("%.3f" , result );
+		
+		
+	}
+
+}
