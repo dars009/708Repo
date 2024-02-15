ac021073cd2ee814710e8a8ffe9ea5bf1cc6cab0
Aanis Pathan <147499408+AAANISPATHAN@users.noreply.github.com>
2024-02-15 08:37:19 +0530

    Update StudentService.java

--- SpringBOOT_04_JpaRepository/src/main/java/com/sb/services/StudentService.java
+++ SpringBOOT_04_JpaRepository/src/main/java/com/sb/services/StudentService.java
@@ -12,10 +12,45 @@ import com.sb.pojo.Student;
 public class StudentService {
 	
 	@Autowired
+
 	StudentDAO studentDao;
 
 	public List<Student> getAllStudent() {
 		return studentDao.findAll();
 	}
 
+	
+	public Student addStudent(Student student) {
+		Student newStudent = new Student();
+		newStudent.setName("Aanis Pathan");
+		newStudent.setCourse("Computer Science");
+		return studentDao.save(student);
+	}
+
+	public Student updateStudent(Integer studentId, Student updatedStudent) {
+		Student existingStudent = studentDao.findById(studentId).orElse(null);
+
+		if (existingStudent != null) {
+			existingStudent.setName(updatedStudent.getName());
+			existingStudent.setCourse(updatedStudent.getCourse());
+
+			return studentDao.save(existingStudent);
+		} else {
+
+			return null;
+		}
+	}
+
+	public Map<String, Boolean> deleteStudentById(Integer studentId) {
+		Optional<Student> optionalStudent = studentDao.findById(studentId);
+
+		if (optionalStudent.isPresent()) {
+			studentDao.deleteById(studentId);
+
+			Map<String, Boolean> result = new HashMap<>();
+			result.put("Student Deleted", Boolean.TRUE);
+			return result;
+		} else
+			return null;
+	}
 }
